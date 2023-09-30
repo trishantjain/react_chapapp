ddb41517c4008625c052957cd39f286ba9be1e1a
trishantjain <trishantjain8888@gmail.com>
2023-10-01 00:04:50 +0530

    Added new Routes in Index.js: -
    - Route for chat
    - Route for chat for particular Id

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const express = require('express')
+const express = require('express');
+const dotenv = require('dotenv');
+const { chats } = require('./data/data');
 
 const app = express();
+dotenv.config();
 
-app.listen(4000, console.log("listning at port 4000"));
+app.get("/api/chat", (req, res) => {
+    res.send(chats)
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+})
+
+const PORT = process.env.PORT
+
+app.listen(PORT, console.log(`listning at port ${PORT}`));
